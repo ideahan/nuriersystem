66483036cefb3a197ba8e152af430948b74d5e4f
DESKTOP-RRIEO8Q\nurier <ideahan@gmail.com>
2022-06-30 09:51:58 +0900

    수정 커밋

--- static/js/common-ui.js
+++ static/js/common-ui.js
@@ -2,13 +2,11 @@ $(document).ready(function () {
     var WinHeight = $(window).height();
     console.log(WinHeight);
 	$(window).scroll(function(){
-       
         if( WinHeight > 114){
             $('#header').addClass('active')
         }
         else {
             $('#header').removeClass('active')
         }
-
     })
 });
